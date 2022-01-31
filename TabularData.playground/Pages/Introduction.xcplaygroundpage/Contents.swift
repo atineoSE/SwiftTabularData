ba76c62a6f4ae575d78803f9679aa7cd6d69c97e
Adrian Tineo <adrian@adriantineo.com>
2022-01-31 11:52:25 +0100

    Add intro

---
+++ TabularData.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+
+/*:
+ # Explore and manipulate data in Swift with TabularData
+ This Xcode playground is based on the [tech talk of the same name ](https://developer.apple.com/videos/play/tech-talks/10100) by Alejandro Isaza and David Findlay, both ML Sotware Engineers at Apple.
+ */
+print("This playground was developed using Swift 5.")
+print("You are using Swift version: \(getSwiftVersion())")
+//: [Next](@next)
---
+++ TabularData.playground/Sources/Utilities.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public func getSwiftVersion() -> String {
+    #if swift(>=5.0)
+    return "5 or higher"
+    #elseif swift(>=4.0)
+    return "4.x"
+    #elseif swift(>=3.0)
+    return "3.x"
+    #else
+    return "lower than 3.0"
+    #endif
+}
